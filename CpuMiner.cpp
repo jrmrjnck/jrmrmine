e4ef1685cb9dd087f24f9ed7205b8a85fef18b3f
Jonathan Doman <jonathan.doman@gmail.com>
2015-05-31 14:52:11 -0500

    Implement basic cpu mining logic

--- CpuMiner.cpp
+++ CpuMiner.cpp
@@ -4,6 +4,8 @@
 #include "Miner.h"
 
 #include <memory>
+#include <limits>
+#include <cassert>
 
 class CpuMiner : public Miner
 {
@@ -14,6 +16,41 @@ public:
    static MinerPtr createInstance()
    {
       return MinerPtr( new CpuMiner );
+   }
+
+protected:
+   virtual Result _mine( const Sha256& preHash, const ByteArray& reverseTarget, uint32_t& nonce )
+   {
+      assert( reverseTarget.size() == sizeof(Sha256::Digest) );
+
+      nonce = 0;
+      do
+      {
+         // Complete the first hash
+         Sha256 hash( preHash );
+         Sha256::Digest digest;
+
+         hash.update( &nonce, sizeof(nonce) * CHAR_BIT );
+         hash.digest( digest );
+
+         // Do it again
+         auto result = Sha256::hash( digest.toByteArray() );
+
+         for( int i = result.size() - 1; i >= 0; --i )
+         {
+            if( result[i] > reverseTarget[i] )
+            {
+               break;
+            }
+
+            if( result[i] < reverseTarget[i] )
+            {
+               return SolutionFound;
+            }
+         }
+      } while( nonce++ < std::numeric_limits<uint32_t>::max() );
+
+      return NoSolutionFound;
    }
 };
 
