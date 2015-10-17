52a70ad9b99d2d63c2a9c40609916ff1d361ea07
Jonathan Doman <jonathan.doman@gmail.com>
2015-10-17 15:38:12 -0700

    Deduplicate help text strings

--- Settings.cpp
+++ Settings.cpp
@@ -17,22 +17,32 @@ namespace BoostProgOpt = boost::program_options;
 
 static BoostProgOpt::variables_map _varMap;
 
+#define OPT_HELP     "help"
+#define OPT_DEBUG    "debug"
+#define OPT_CONFIG   "config"
+#define OPT_TYPE     "type"
+
+#define OPT_RPCHOST     "rpchost"
+#define OPT_RPCPORT     "rpcport"
+#define OPT_RPCPASSWORD "rpcpassword"
+#define OPT_RPCUSER     "rpcuser"
+
 void Settings::init( int argc, char** argv )
 {
    BoostProgOpt::options_description generalOptions( "General Options" );
    generalOptions.add_options()
-      ("help,h", "Print this help.")
-      ("debug,d", "Show debug output.")
-      ("config,c", BoostProgOpt::value<string>()->default_value(defaultConfigFile()), "Bitcoin Core configuration file to load.")
-      ("type,t", BoostProgOpt::value<string>()->default_value("cpu"), typeHelpText().c_str())
+      (OPT_HELP",h",    "Print this help.")
+      (OPT_DEBUG",d",   "Show debug output.")
+      (OPT_CONFIG",c",  BoostProgOpt::value<string>()->default_value(defaultConfigFile()), "Bitcoin Core configuration file to load.")
+      (OPT_TYPE",t",    BoostProgOpt::value<string>()->default_value("cpu"), typeHelpText().c_str())
       ;
 
    BoostProgOpt::options_description coreOptions( "Bitcoin Core Options" );
    coreOptions.add_options()
-      ("rpchost", BoostProgOpt::value<string>()->default_value("http://localhost"))
-      ("rpcport", BoostProgOpt::value<int>()->default_value(18332))
-      ("rpcpassword", BoostProgOpt::value<string>())
-      ("rpcuser", BoostProgOpt::value<string>())
+      (OPT_RPCHOST,     BoostProgOpt::value<string>()->default_value("http://localhost"))
+      (OPT_RPCPORT,     BoostProgOpt::value<int>()->default_value(18332))
+      (OPT_RPCPASSWORD, BoostProgOpt::value<string>())
+      (OPT_RPCUSER,     BoostProgOpt::value<string>())
       ;
 
    BoostProgOpt::options_description allOptions;
@@ -42,36 +52,36 @@ void Settings::init( int argc, char** argv )
    BoostProgOpt::store( BoostProgOpt::parse_command_line(argc,argv,allOptions), _varMap );
    BoostProgOpt::notify( _varMap );
 
-   if( _varMap.count("help") )
+   if( _varMap.count(OPT_HELP) )
    {
       cout << allOptions;
       exit( 0 );
    }
 
    // Read settings from configuration file, ignoring unknown settings
-   const auto& configFile = _varMap["config"].as<string>();
+   const auto& configFile = _varMap[OPT_CONFIG].as<string>();
    BoostProgOpt::store( BoostProgOpt::parse_config_file<char>(configFile.c_str(),coreOptions,true), _varMap );
    BoostProgOpt::notify( _varMap );
 }
 
 std::string Settings::RpcHost()
 {
-   return _varMap["rpchost"].as<string>();
+   return _varMap[OPT_RPCHOST].as<string>();
 }
 
 int Settings::RpcPort()
 {
-   return _varMap["rpcport"].as<int>();
+   return _varMap[OPT_RPCPORT].as<int>();
 }
 
 std::string Settings::RpcUser()
 {
-   return _varMap["rpcuser"].as<string>();
+   return _varMap[OPT_RPCUSER].as<string>();
 }
 
 std::string Settings::RpcPassword()
 {
-   return _varMap["rpcpassword"].as<string>();
+   return _varMap[OPT_RPCPASSWORD].as<string>();
 }
 
 std::string Settings::defaultConfigFile()
@@ -103,10 +113,10 @@ std::string Settings::typeHelpText()
 
 const std::string& Settings::minerType()
 {
-   return _varMap["type"].as<std::string>();
+   return _varMap[OPT_TYPE].as<std::string>();
 }
 
 bool Settings::debug()
 {
-   return _varMap.count( "debug" );
+   return _varMap.count( OPT_DEBUG );
 }
